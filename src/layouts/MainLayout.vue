a226874de58230d2eefc046faa13d4bc5fe935f9
Gabriel Morais <gabrieldemorais6@gmail.com>
2022-04-30 21:46:09 -0300

    Refactoring menu links

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,49 +16,22 @@
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Menu </q-item-label>
+      <q-item-label header> Menu </q-item-label>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <MenuLinks />
     </q-drawer>
 
     <q-page-container
       class="relative-position row flex-center"
       style="width: 100%"
     >
-      <router-view class="col-xs-8 col-xl-8" />
+      <router-view class="col-xs-12 col-md-12 col-lg-8" />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from "components/EssentialLink.vue";
-
-const linksList = [
-  {
-    title: "Meus alarmes",
-    caption: "Quando devo tomar meu próximo remédio?",
-    icon: "access_alarm",
-    link: "#/",
-  },
-  {
-    title: "Adicionar alarme",
-    caption: "Adicionar um alarme para um remédio",
-    icon: "add_alarm",
-    link: "#/add-alarm",
-  },
-  {
-    title: "Histórico de alarmes",
-    caption: "Visualizar histórico de alarmes",
-    icon: "history",
-    link: "#/alarm-history",
-  },
-];
+import MenuLinks from "components/MenuLinks.vue";
 
 import { defineComponent, ref } from "vue";
 
@@ -66,14 +39,13 @@ export default defineComponent({
   name: "MainLayout",
 
   components: {
-    EssentialLink,
+    MenuLinks,
   },
 
   setup() {
     const leftDrawerOpen = ref(false);
 
     return {
-      linksList,
       leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
